6ad5247ce1c8b2f3929601aefa814ae87f6772b6
harshit1200 <105700811+harshit1200@users.noreply.github.com>
2022-05-30 21:35:59 +0530

    code of problem 2

--- MainActivity.java
+++ MainActivity.java
@@ -1,20 +1,24 @@
-package com.example.sonali_app1;
+package com.example.sonali_app3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    public void load(View view){
-        Button button = findViewById(R.id.button);
-        TextView textView = findViewById(R.id.textView2);
-        button.setText(textView.getText().toString());
+    boolean isegg=true;
+    public void change(View view){
+        ImageView iv = findViewById(R.id.imageView);
+        if(isegg){
+            iv.setImageResource(R.drawable.cat);
+            isegg=false;
+        }else{
+            iv.setImageResource(R.drawable.egg2);
+            isegg=true;
+        }
+    }
 
-
-}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
